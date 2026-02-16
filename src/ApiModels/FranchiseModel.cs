3cc5553a46e6ac8240666c8615d78d4e75967858
Maksim <m_telega@mail.ru>
2026-02-16 13:46:17 +0300

    chore: update FranchiseModel.cs

--- src/ApiModels/FranchiseModel.cs
+++ src/ApiModels/FranchiseModel.cs
@@ -12,20 +12,9 @@
 
         public decimal? Rating { get; set; } = null;
 
-        private int? _lastYear;
-        public int LastYear
-        {
-            get => _lastYear ?? 0;
-            set => _lastYear = value;
-        }
-    
-        private int? _firstYear;
-        public int FirstYear
-        {
-            get => _firstYear ?? 0;
-            set => _firstYear = value;
-        }
-
+        public int? LastYear { get; set; }
+        public int? FirstYear { get; set; }
+        
         public int TotalReleases { get; set; }
 
         public int TotalEpisodes { get; set; }
